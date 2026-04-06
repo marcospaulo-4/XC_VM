9abd0c66741891b515b9e655948671e89ca47a13
Divarion-D <danil.dmitrievn@gmail.com>
2026-04-06 19:37:40 +0300

    fix(api): handle null from probeSource() to prevent array offset on null

--- src/public/Views/admin/api.php
+++ src/public/Views/admin/api.php
@@ -2500,8 +2500,9 @@ if (isset($_SESSION['hash'])) {
 					}
 
 					if (!$rStreamInfo) {
-						$rStreamInfo = ServerRepository::probeSource($rServerID, RequestManager::getAll()['url'], (RequestManager::getAll()['user_agent'] ?? null), (RequestManager::getAll()['http_proxy'] ?? null), (RequestManager::getAll()['cookies'] ?? null), (RequestManager::getAll()['headers'] ?? null))['data'];
-						$rStreamInfo['container'] = $rStreamInfo['format']['format_name'];
+						$rProbeResult = ServerRepository::probeSource($rServerID, RequestManager::getAll()['url'], (RequestManager::getAll()['user_agent'] ?? null), (RequestManager::getAll()['http_proxy'] ?? null), (RequestManager::getAll()['cookies'] ?? null), (RequestManager::getAll()['headers'] ?? null));
+						$rStreamInfo = $rProbeResult['data'] ?? [];
+						$rStreamInfo['container'] = $rStreamInfo['format']['format_name'] ?? '';
 					}
 				}
 
